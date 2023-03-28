2569fe052d9833ef3a40137d797084a194dba58c
enfzrhbveivberv <evann.semens@student.junia.com>
2023-03-28 16:52:32 +0200

    add files

--- script.py
+++ script.py
@@ -1,24 +1,23 @@
 from openpyxl import load_workbook
 from tkinter import * 
-
+from tkinter.filedialog import *
 
 
 #Toutes les fonctions :
 # Fonction pour modifier le excel
 def import_excel():
     #on charge le fichier excel
-    nom_fichier = texte.get("1.0", "end-1c")
+   # nom_fichier = texte.get("1.0", "end-1c")
     #on rajoute l'extenssion .xlsx
-    nom_fichier = nom_fichier + ".xlsx"
-    workbook = load_workbook(filename=nom_fichier)
+    files = askopenfilename(title="Ouvrir un fichier",filetypes=[('xlsx files','.xlsx'),('all files','.*')])
+    #nom_fichier = nom_fichier + ".xlsx"
+    workbook = load_workbook(filename=files)
     #on ouvre le fichier excel
     sheet = workbook.active
     #on modifie la cellule A1
     sheet["A1"] = "ca change"
     #on sauvegarde le fichier
-    workbook.save(filename=nom_fichier)
-
-
+    workbook.save(filename="value.xlsx")
 
     
     
@@ -90,13 +89,13 @@ texte.config(width=15, height=1)
 texte.pack()
 #on pre-ecrit du texte dans la zone de texte
 texte.insert(END, "nom du fichier")
-#modifier pour plutot mettre un bouton qui ouvre l'explorateur de fichier windows
 
 
 
 #bouton qui lance l'importation seulement quand on appuie dessus
 boutton = Button(fenetre, text="Importer", command=import_excel)
 boutton.pack()
+
 
 
   
@@ -112,7 +111,7 @@ switch.place(x=0, y=0)
 
 
 #on ouvre fenetre en plein ecran
-#fenetre.attributes('-fullscreen', True)
+fenetre.attributes('-fullscreen', True)
 
 fenetre.mainloop()
 
