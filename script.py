16b6f41ff4f5e33f99ffdaccba03ae712b69a89a
Serval <logan.doceul@student.junia.com>
2023-03-28 08:52:56 +0200

    Upgrade theme change
    Changement de l'ordre du fichier et amelioration du changement de theme

--- script.py
+++ script.py
@@ -31,41 +31,9 @@ light = light.subsample(30, 30) #on redefinit leurs tailles
 dark = PhotoImage(file="dark.png")
 dark = dark.subsample(30, 30) #on redefinit leurs tailles
 switch_value = True
-  
-# Fonction pour changer le theme
-def toggle():
-  
-    global switch_value
-    if switch_value == True:
-        switch.config(image=dark, bg="#26242f",
-                      activebackground="#26242f")
-          
-        #met la fenetre en noir
-        fenetre.config(bg="#26242f") 
-        switch_value = False
-  
-    else:
-        switch.config(image=light, bg="white", 
-                      activebackground="white")
-          
-        #met la fenetre en blanc
-        fenetre.config(bg="white") 
-        switch_value = True
-  
-  
-# Bouton pour changer le theme
-switch = Button(fenetre, image=light, 
-                bd=0, bg="white",
-                activebackground="white", 
-                command=toggle)
-switch.pack(padx=50, pady=150)
-#on le place en haut a droite
-switch.place(x=0, y=0)
 
 
 
-#on ouvre fenetre en plein ecran
-fenetre.attributes('-fullscreen', True)
 
 #on demande les données de tailles de la fenetre
 largeur = fenetre.winfo_screenwidth()
@@ -83,6 +51,57 @@ label.pack()
 boutton = Button(fenetre, text="importer vos données", command=import_excel)
 boutton.place(x=200, y=200)
 boutton.pack()
+
+
+  
+# Fonction pour changer le theme
+def toggle():
+  
+    global switch_value
+    if switch_value == True:
+        switch.config(image=dark, bg="#26242f",
+                      activebackground="#26242f")
+          
+        #met la fenetre en noir
+        fenetre.config(bg="#26242f") 
+        switch_value = False
+        #suprime le label deja existant
+        #a ajouter
+        label=Label(fenetre, text="Bienvenue dans S-EAU-L", bg="#26242f", fg="white")
+        label.pack()
+        
+  
+    else:
+        switch.config(image=light, bg="white", 
+                      activebackground="white")
+          
+        #met la fenetre en blanc
+        fenetre.config(bg="white") 
+        switch_value = True
+        #suprime le label deja existant
+        #a ajouter
+        label=Label(fenetre, text="Bienvenue dans S-EAU-L", bg="white", fg="black")
+        label.pack()
+
+        
+  
+  
+# Bouton pour changer le theme
+switch = Button(fenetre, image=light, 
+                bd=0, bg="white",
+                activebackground="white", 
+                command=toggle)
+switch.pack(padx=50, pady=150)
+#on le place en haut a droite
+switch.place(x=0, y=0)
+
+
+
+#on ouvre fenetre en plein ecran
+fenetre.attributes('-fullscreen', True)
+
+
+
 
 
 
