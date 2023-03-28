435264189a2373d0ec1d735c383c817052302a93
Serval <logan.doceul@student.junia.com>
2023-03-28 11:24:29 +0200

    Main.py mis a jour
    Le main.py installe maintenant les packages nécessaires, et run le script.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Logan is in the place")
+import os
+#change terminal color
+os.system("color 0a")
+
+#on installe time
+os.system("pip install time")
+import time
+
+time.sleep(1)
+#on installe tkinter
+os.system("pip install tk")
+
+time.sleep(5)
+#os.system("python script.py")
+#import script
+
+
