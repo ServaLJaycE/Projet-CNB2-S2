133ca5b8bbde6837103d0cc6614562b660edf7f5
Serval <logan.doceul@student.junia.com>
2023-04-01 02:40:30 +0200

    Main.py couleurs fix
    -ca s'exécute entièrement en vert maintenant

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ os.system("color 0a")
 os.system("pip install time")
 import time
 
+os.system("color 0a")
 time.sleep(1)
 #on installe tkinter
 os.system("pip install tk")
 
+os.system("color 0a")
 time.sleep(5)
 #os.system("python script.py")
 import script
