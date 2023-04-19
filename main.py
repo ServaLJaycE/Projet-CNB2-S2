31b20081ed2c7a7fa591c9a814cedbb4b3de0a13
Serval <logan.doceul@student.junia.com>
2023-04-19 19:19:41 +0200

    Ajout des import
     Ajout des nouveaux import dans le main

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import os
+
+#tester reseau, si non -> fenetre .vbs alerte reseau non connecté 
+
+
+
+
+
 #change terminal color
 os.system("color 0a")
 
@@ -6,13 +13,18 @@ os.system("color 0a")
 os.system("pip install time")
 import time
 
-os.system("color 0a")
 time.sleep(1)
+os.system("color 0a")
 #on installe tkinter
 os.system("pip install tk")
+time.sleep(5)
+
 
 os.system("color 0a")
+#on installe matplotlib
+os.system("pip install matplotlib")
 time.sleep(5)
+os.system("color 0a")
 #os.system("python script.py")
 import script
 
